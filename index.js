552f0c17354244235055c2f5d2b362c46938f4f9
Benjamin <benjaminrowe0@gmail.com>
2022-01-09 16:30:47 +1100

    have fixed the problem

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+
+let findMatching = (drivers,name) => {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+
+let fuzzyMatch = (drivers, name) => {
+    return drivers.filter((driver) => {
+        return driver.startsWith(name)
+    })
+}
+
+let matchName = (drivers, name) => {
+    return drivers.filter((driver) => {
+        return driver.name === name
+    })
+}
+
+// console.log(findMatching(drivers,"Bobby"))
+// console.log(fuzzyMatch(drivers,"A"))
+console.log(matchName(drivers, "Bobby"))
